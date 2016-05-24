5db2563337f60a162d1101f605e75f8ce10313aa
great <great@DESKTOP-1UU63T4>
2016-05-24 12:44:47 +0700

    APPVARS Change vars

--- phplib/APPVARS.php
+++ phplib/APPVARS.php
@@ -7,9 +7,9 @@ define("DBPORT", 34600);
 
 define("PHPMAILER_FROM", 'noreply@icodeformoney.com');
 define("PHPMAILER_PASS", "Haha23235");
-define("PHPMAILER_NAME", 'So-laris noreply');
+define("PHPMAILER_NAME", 'SSBIN noreply');
 define("PHPMAILER_HOST", 'lax215.arandomserver.com');
 define("PHPMAILER_PORT", 26);
 
 define("APPNAME", "SSBIN");
-define("DOMAIN", "https://darah.loc");
+define("DOMAIN", "https://ssbin.loc");
