add67ec2344691eb9eec294a1c69c1727848d3cb
great <great@DESKTOP-1UU63T4>
2016-05-27 11:11:41 +0700

    The users change password module

--- php/controller/users/password.php
+++ php/controller/users/password.php
@@ -2,41 +2,20 @@
 if (!count($_POST)) { include DIR."/php/view/users/password.php"; die(); }
 
 use Trust\JSONResponse;
-use Solaris\Pengguna;
+use SSBIN\User;
 use Trust\Basic;
-$services = ['pass','setUser','cekUser'];
+$services = ['pass'];
 if (in_array($_POST['a'], $services)) $_POST['a']();
 
 function pass() { global $login;
   $pass = json_decode(json_encode($_POST['pass']));
-  $p = Pengguna::find($login->id, "id,password,data_info");
-  if ($p->password && hash('sha256',$pass->oldpass) != $p->password) JSONResponse::Error("Password lama tidak benar");
+  $p = User::find($login->id, "id,password,data_info");
+  if ($p->password && hash('sha256',$pass->oldpass) != $p->password) JSONResponse::Error("Wrong old password ");
   if ($pass->newpass != $pass->repass) JSONResponse::Error ("Konfirmasi password tidak sama");
   $err = Basic::validatePass($pass->newpass);
-  if (count($err)) JSONResponse::Error(implode("<br />", $err));
+  if (count($err)) JSONResponse::Error(implode("\n", $err));
   
   $p->password = hash('sha256', $pass->newpass);
   $p->save();
-  JSONResponse::Success();
-}
-
-function cekUser() {global $login;
-  if ($login->username) JSONResponse::Error("Username tidak dapat diganti");
-  $err = Pengguna::validateUsername($_POST['user']);
-  if (count($err)) JSONResponse::Success(["res"=>"not OK","message"=>$err[0]]);
-  JSONResponse::Success(['res'=>'OK']);
-}
-
-function setUser() { global $login;
-  if ($login->username) JSONResponse::Error("Username tidak dapat diganti");
-  $err = Pengguna::validateUsername($_POST['user']);
-  if (count($err)) JSONResponse::Error(implode("<br />",$err));
-  $p = Pengguna::find($login->id, "id,username,data_info");
-  $login->username = $p->username = $_POST['user'];
-  $p->save();
-  
-  $login->data_info= $p->data_info;
-  $_SESSION['login'] = $login;
-  
-  JSONResponse::Success(['res'=>'OK']);
+  JSONResponse::Success(["message"=>"Password successfully changed"]);
 }
--- php/view/users/password.php
+++ php/view/users/password.php
@@ -3,42 +3,17 @@ $pageTitle = "Password";
 $pageSubTitle = "Ganti Password";
 include $template;
 function htmlHead() { ?>
+<script>token = '<?= \Trust\Server::csrf_token(); ?>';</script>
 <script>
   app.controller('ctrlPass',function($scope) {
-    token = '<?= \Trust\Server::csrf_token(); ?>';
-    var timeout;
-    $scope.username = '';
     $scope.pass = {oldpass:'',newpass:'',repass:''};
     $scope.ubah = function() {
       if ($scope.pass.newpass !== $scope.pass.repass) {
-        Example.show("Konfirmasi password tidak sama", "danger"); return;
+        $.notify("Password confirmation invalid", "error"); return;
       }
       oPost = {a:'pass',pass:$scope.pass,token:token};
       tr.post("/users/password",oPost,function(rep) {
-        Example.show("Password telah diubah","success");
-      });
-    };
-    $scope.userChanged = function() {
-      clearTimeout(timeout);
-      if ($scope.username == '') return;
-      timeout = setTimeout($scope.cekUser,1000);
-    };
-    $scope.cekUser = function() {
-      clearTimeout(timeout);
-      tr.post("/users/password",{a:'cekUser',user:$scope.username,token:token},function(rep) {
-        if (rep.res === "OK") {
-          $scope.userMessage = "Username OK"; $scope.msgClass="text-success";
-        } else {
-          $scope.userMessage = rep.message; $scope.msgClass="text-danger";
-        }
-        $scope.$apply();
-      });
-    };
-    $scope.changeUser = function() {
-      clearTimeout(timeout);
-      tr.post("/users/password",{a:'setUser',user:$scope.username,token:token},function(rep) {
-        Example.show("Username telah diubah","success");
-        $("#panelUser").html('');
+        $.notify(rep.message);
       });
     };
   });
@@ -48,47 +23,25 @@ function htmlHead() { ?>
 function mainContent() { global $login; ?>
 <div class="row" ng-controller="ctrlPass">
   <div class="col-md-4 col-md-offset-4">
-    <?php if ($login->kontak->fbid) { ?>
-    <div class="alert alert-info flex flex-vcenter">
-      <i class="fa fa-info-circle fa-2x" style="margin: 0 5px 0 0;"></i>
-      Anda tidak memerlukan username dan password bila login dengan facebook
-    </div>
-    <?php if (!$login->username) { ?>
-    <div class='panel panel-info' id='panelUser'>
-      <div class="panel-heading"><h3 class="panel-title">Buat username</h3></div>
-      <div class='panel-body'>
-        <i class="fa fa-info-circle"></i> Username hanya dapat dipilih satu kali
-        <form>
-          <div class='form-group'>
-            <input type='text' class='form-control' placeholder="Username" ng-model="username" ng-change='userChanged()' />
-            <span ng-class="msgClass">{{userMessage}}</span>
-          </div>
-          <button ng-click='cekUser()' class='btn btn-info'>Cek Username</button>
-          <button ng-click='changeUser()' class='btn btn-success'>Pakai username</button>
-        </form>
-      </div>
-    </div>
-    <?php } } ?>
     <div class="panel panel-default">
       <div class='panel-heading'>
-        <h3 class="panel-title">Ubah Password</h3>
+        <h3 class="panel-title">Change password</h3>
       </div>
       <div class='panel-body'>
         <form>
           <div class="form-group">
-            <label for="oldpass">Password Lama</label>
-            <input type="password" class="form-control" ng-model="pass.oldpass" placeholder="Password lama" />
-            <span class='text-info'>Kosongkan bila belum ada</span>
+            <label for="oldpass">Old Password</label>
+            <input type="password" class="form-control" ng-model="pass.oldpass" placeholder="Old Password" />
           </div>
           <div class="form-group">
-            <label for="newpass">Password Baru</label>
-            <input type="password" class="form-control" ng-model="pass.newpass" placeholder="Password baru" />
+            <label for="newpass">New Password</label>
+            <input type="password" class="form-control" ng-model="pass.newpass" placeholder="New Password" />
           </div>
           <div class="form-group">
-            <label for="repass">Ulangi Password</label>
-            <input type="password" class="form-control" ng-model="pass.repass" placeholder="Ulangi password baru" />
+            <label for="repass">Repeat Password</label>
+            <input type="password" class="form-control" ng-model="pass.repass" placeholder="Repeat new password" />
           </div>
-          <button ng-click="ubah()" class='btn btn-primary'>Ubah Password</button>
+          <button ng-click="ubah()" class='btn btn-primary'>Change password</button>
         </form>
       </div>
     </div>
