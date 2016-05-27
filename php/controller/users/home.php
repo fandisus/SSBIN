98e5d1b22e4f987e1c5df028055cc0dd03999ad2
great <great@DESKTOP-1UU63T4>
2016-05-27 12:39:41 +0700

    User home module for later.

---
+++ php/controller/users/home.php
@@ -0,0 +1,5 @@
+<?php
+if (!count($_POST)) { include DIR."/php/view/users/home.php"; die(); }
+
+//TODO: Home user
+
---
+++ php/view/users/home.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = "Users Home";
+include $template;
+
+function htmlHead() {
+  
+}
+
+function mainContent() {
+  echo "halaman home: inbox, confirmed data input<br />For experts: New validation requests";
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
