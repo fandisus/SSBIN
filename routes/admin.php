9ade6f3b827adb5d92fa7869c67ba3d7c84e919e
great <great@DESKTOP-1UU63T4>
2016-06-03 13:28:07 +0700

    Refactor admin and user routes.

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,13 @@
 <?php
+if (!isset($login) || $login->level != \SSBIN\User::USER_ADMIN) header("location:/");
 $ctrlDir = DIR."/php/controller";
-$services = ["login","logout","admins","experts"];
-if ($paths[0] == "") include "$ctrlDir/common/home.php";
-elseif (in_array($paths[0], $services)) include "$ctrlDir/common/$paths[0].php";
+$template = DIR."/php/view/admin/tulang.php";
+$services = [
+    "users", //admins, experts, standard, validate,
+    "organizations", //categories, organizations
+    "taxonomies", //kingdom ---> species
+    "database" //backup, restore, export
+    ];
+if (!isset($paths[1]) || $paths[1]=="") include "$ctrlDir/admin/home.php";
+elseif (in_array($paths[1], $services)) include "$ctrlDir/admin/$paths[1].php";
 else { include "$ctrlDir/404.php"; die(); }
--- routes/users.php
+++ routes/users.php
@@ -1,12 +1,13 @@
 <?php
 if (!isset($login)) header("location:/");
+//if (!$login->active) {
+//  if (isset($paths[1])) header("location:/users");
+//}
+
 $ctrlDir = DIR."/php/controller";
 $template = DIR."/php/view/tulang.php";
 $services = ["password","profile"];
 
 if (!isset($paths[1]) || $paths[1]=="") include "$ctrlDir/users/home.php";
-elseif (in_array($paths[1],$services)) {
-  if (!$login->active) header("location:/users");
-  include "$ctrlDir/users/$paths[1].php";
-}
+elseif (in_array($paths[1],$services)) include "$ctrlDir/users/$paths[1].php";
 else { include "$ctrlDir/404.php"; die(); }
