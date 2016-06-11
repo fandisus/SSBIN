a13aa321090a9d94eee36a33cabfb7edd3046a8e
great <great@DESKTOP-1UU63T4>
2016-06-11 12:19:07 +0700

    Admin users module

--- php/controller/admin/users.php
+++ php/controller/admin/users.php
@@ -3,5 +3,29 @@ if (!count($_POST)) { include DIR."/php/view/admin/users.php"; die(); }
 
 use Trust\JSONResponse;
 use SSBIN\User;
-$services = ['del'];
+$services = ['toggleValidation','save'];
 if (in_array($_POST['a'], $services)) $_POST['a']();
+
+function toggleValidation() { global $login;
+  if (!isset($_POST['id']) || !is_numeric($_POST['id'])) JSONResponse::Error("User id error");
+  $id = $_POST['id'];
+  if ($login->id == $id) JSONResponse::Error("Tidak bisa validasi diri sendiri");
+  $user = User::find($id,"id,validated,data_info");
+  $user->validated = !$user->validated;
+  $user->save();
+  JSONResponse::Success(["validated"=>$user->validated]);
+}
+
+function save() { global $login;
+  if (!isset($_POST['u'])) JSONResponse::Error("Error sending user data");
+  $u = json_decode(json_encode($_POST['u']));
+  if ($u->id == $login->id && $u->level != $login->level) JSONResponse::Error ("Can't alter self's level.");
+  $user = User::find($u->id);
+  if ($u->id == 1 && $u->level != $user->level) JSONResponse::Error ("Can't alter first user's level");
+  $user->level = $u->level;
+  $user->category = $u->category;
+  $user->organization = $u->organization;
+  $user->save();
+  
+  JSONResponse::Success(['message'=>'Saved successfully','o'=>$u]);
+}
---
+++ public/js/admin/users.js
@@ -0,0 +1,54 @@
+app.controller("ctrlUsers",function($scope) {
+  var init = JSON.parse($("#init").html());
+  $("#init").html("");
+  $scope.users = init.users;
+  $scope.levels = init.levels;
+  $scope.categories = init.categories;
+  $scope.organizations = [];
+  $scope.u = {};
+  $scope.editUser = {};
+  $scope.updateOrganizationSelect = function() {
+    $scope.organizations = $scope.categories[$scope.editUser.category];
+  };
+  $scope.toggleValidation = function(u) {
+    var oPost = {a:"toggleValidation",id:u.id,token:token};
+    tr.post("/admin/users",oPost,function(rep) {
+      u.validated = rep.validated;
+      $scope.$apply();
+    });
+  };
+  $scope.momenter = function(d) {
+    return moment(d).calendar();
+  };
+  $scope.phoneLists = function(u) {
+    if (u.biodata == undefined) return;
+    var res = "";
+    for (var i in u.biodata.phone) {
+      res += u.biodata.phone[i] + "\n";
+    }
+    return res;
+  };
+  $scope.show = function(u) {
+    $scope.u = u;
+    $("#modalShowUser").modal('show');
+  };
+  $scope.edit = function(u) {
+    $scope.u = u;
+    $scope.editUser = angular.copy(u);
+    $scope.updateOrganizationSelect();
+    $("#modalEdit").modal("show");
+  };
+  $scope.saveChanges = function(eu) {
+    var obj = {id:eu.id,level:eu.level,category:eu.category,organization:eu.organization};
+    var oPost = {a:'save',u:obj,token:token};
+    tr.post("/admin/users",oPost,function(rep) {
+      $scope.u.level = rep.o.level;
+      $scope.u.category = rep.o.category;
+      $scope.u.organization = rep.o.organization;
+      $scope.$apply();
+      //TODO: Edit level diri sendiri harus hati-hati, terutama ID 1
+      $("#modalEdit").modal('hide');
+      $.notify(rep.message,"success");
+    });
+  };
+});
