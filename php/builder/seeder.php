efa77d0a2db7eba79db659f829cadb286ea2d2ba
great <great@DESKTOP-1UU63T4>
2016-06-20 17:43:19 +0700

    Seeder organization, add individual in it.

--- php/builder/seeder.php
+++ php/builder/seeder.php
@@ -1,15 +1,14 @@
 <?php
-//seedOrganizations();
+seedOrganizations();
 function seedOrganizations() {
   $cats = [
       "Government"=>["BLHD","Forestry"],
       "Corporations"=>["PT REKI","PT Sinar Mas"],
       "Academics"=>["UNSRI","UMP"],
       "NGO"=>['WBH'],
-      "Individual"
+      "Individual"=>['']
   ];
   foreach ($cats as $cat=>$orgs) {
-    if (!is_array($orgs)) continue;
     foreach ($orgs as $org) {
       $obj = new \SSBIN\Organization(["name"=>$org, "category"=>$cat]);
       $obj->save();
