9320fa6e995d2a033368343b88e403d46022288a
great <great@DESKTOP-1UU63T4>
2016-06-21 10:18:38 +0700

    Public Index, hide sensitive data from admin

--- public/index.php
+++ public/index.php
@@ -20,15 +20,27 @@ if (isset($_SESSION['login'])) {
   $_ENV['USER'] = null;
 }
 
+//dari .htaccess: subdo dan path.
+$paths = explode("/",$_GET['path']);
+
+if (count($_POST)) { //hide sensitive data even from admin
+  if ($paths[0] == 'login') $postData = ['a'=>'login','user'=>'secret'];
+  elseif ($paths[0] == 'users' && $paths[1] == 'password') $postData = ['a'=>'pass','pass'=>'secret'];
+  elseif ($paths[0] == 'register' && $_POST['a'] == 'register') 
+    $postData = [
+        'a'=>'register',
+        'user'=>$_POST['user']['username'],
+        'name'=>$_POST['user']['name']
+      ];
+  else $postData = $_POST;
+}
 $log = new \SSBIN\Logger([
     "ip_address"=>$_SERVER['REMOTE_ADDR'],
     "time"=>date("Y-m-d H:i:s"),
     "requested_url"=>$_SERVER['REQUEST_URI'],
     "userid"=>(isset($login)) ? $login->id : null,
-    "post"=>(count($_POST)) ? $_POST : null
+    "post"=>(count($_POST)) ? $postData : null
   ]);
 $log->save();
 
-//dari .htaccess: subdo dan path.
-$paths = explode("/",$_GET['path']);
 include "../routes/routes.php";
