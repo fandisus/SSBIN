149b74d5356081f27f1c0a6895980c1786f70bbd
great <great@DESKTOP-1UU63T4>
2016-06-22 21:19:23 +0700

    Modul taxonomy classes untuk admin.

---
+++ php/controller/admin/classes.php
@@ -0,0 +1,53 @@
+<?php
+if (!count($_POST)) { include DIR."/php/view/admin/classes.php"; die(); }
+
+use Trust\JSONResponse;
+use SSBIN\Classes;
+use Trust\Forms;
+
+$services = ['saveNew','saveOld','delete'];
+if (in_array($_POST['a'], $services)) $_POST['a'](); else JSONResponse::Error("Service unavailable");
+
+function saveNew() { global $login;
+  $o = Forms::getPostObject('o');
+  if (trim($o->class) == '') JSONResponse::Error("Class cannot be empty");
+  $old = Classes::find($o->class);
+  if ($old != null) JSONResponse::Error("The class $o->class already exists");
+
+  $o = new Classes($o);
+  $o->save();
+
+  JSONResponse::Success(['message'=>"Data successfully added", "new"=>$o]);
+}
+
+function saveOld() {
+  $o = Forms::getPostObject('o');
+  $target = Forms::getPostObject('target');
+  if ($o->class == $target->class) JSONResponse::Error("Class unchanged");
+  if (trim($o->class) == '') JSONResponse::Error("Class cannot be empty");
+  
+  $check = Classes::find($o->class);
+  if ($check != null) JSONResponse::Error("The class $o->class already exists");
+
+  $update = Classes::find($target->class);
+  $update->class = $o->class;
+  $update->setTimestamps();
+  //Note: Belum bisa $update->save() karena mau update PK juga.
+  \Trust\DB::exec('UPDATE classes SET class=:class, data_info=:info WHERE class=:target',
+    [
+      'class'=>$o->class,
+      'target'=>$target->class,
+      'info'=>json_encode($update->data_info)
+    ]);
+  //TODO: Update data findings
+
+  JSONResponse::Success(['message'=>"Data successfully updated",'o'=>$update]);
+}
+
+function delete() {
+  $o = Forms::getPostObject('o');
+  
+  Classes::delete($o->class);
+  //TODO: setnull data findings
+  JSONResponse::Success(["message"=>'Category/Organization deleted successfully']);
+}
---
+++ public/js/admin/classes.js
@@ -0,0 +1,57 @@
+app.controller('ctrlClass',function($scope) {
+  var init = JSON.parse($("#init").html());
+  $scope.classes = init.classes;
+  $scope.classList = []; //load via ajax
+  
+  $scope.target = null; $scope.o = {};
+  $scope.printDataInfo = printDataInfo;
+  $('[data-toggle="popover"]').popover();
+  
+  $scope.newO = function() {
+    $scope.target = null;
+    $scope.modalTitle = "New Class";
+    $scope.o = {class:''};
+    $("#modalEdit").modal('show');
+  };
+  $scope.edit = function(o) {
+    $scope.target = o;
+    $scope.modalTitle = "Edit class: " + o.class;
+    $scope.o = angular.copy(o);
+    $("#modalEdit").modal('show');
+  };
+  $scope.saveChanges = function(o) {
+    if ($scope.target == null) saveNew(o); else $scope.saveOld(o);
+  };
+  var saveNew = function(o) {
+    var oPost = {a:'saveNew',o:o,token:token};
+    tr.post("/admin/taxonomies/classes",oPost, function(rep) {
+      $scope.classes.push(rep.new);
+      $.notify(rep.message,"success");
+      $("#modalEdit").modal('hide');
+      $scope.$apply();
+    });
+  };
+  $scope.saveOld = function(o) {
+    var oPost = {a:'saveOld',o:o,target:$scope.target,token:token};
+    tr.post("/admin/taxonomies/classes",oPost, function(rep) {
+      $scope.target.class = rep.o.class;
+      $scope.target.data_info = rep.o.data_info;
+      $.notify(rep.message,"success");
+      $("#modalEdit").modal('hide');
+      $scope.$apply();
+    });
+  };
+  $scope.delete = function(o) {
+    if (confirm("Are you sure you want to delete?\n\Class: " + o.class) == false) return;
+    var oPost={a:"delete",o:$scope.target,token:token};
+    tr.post("/admin/taxonomies/classes",oPost, function(rep) {
+      $scope.classes.remove($scope.target);
+      $("#modalEdit").modal('hide');
+      $.notify(rep.message,"success");
+      $scope.$apply();
+    });
+  };
+  $scope.oChanged = function() {
+    console.log($scope.o);
+  };
+});
