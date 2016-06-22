4d23e73714a62746b2b2768d53fc47c61028f2c4
great <great@DESKTOP-1UU63T4>
2016-06-22 21:15:58 +0700

    Model iucn dan indo status, tambah $key_name

--- php/model/IUCN_status.php
+++ php/model/IUCN_status.php
@@ -2,6 +2,7 @@
 namespace SSBIN;
 use \Trust\DB;
 class IUCN_status extends \Trust\Model{
+  protected static $key_name="abbr";
   protected static $table_name = "iucn_status", $increment=false, $hasTimestamps = true;
   protected static $json_columns = ['data_info'];
 }
--- php/model/Indo_status.php
+++ php/model/Indo_status.php
@@ -2,6 +2,7 @@
 namespace SSBIN;
 use \Trust\DB;
 class Indo_status extends \Trust\Model{
+  protected static $key_name="abbr";
   protected static $table_name = "indo_status", $increment=false, $hasTimestamps = true;
   protected static $json_columns = ['data_info'];
 }
