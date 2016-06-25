510b97be25e5d8f8f3f2b01d512b0ca4439dcaf9
great <great@DESKTOP-1UU63T4>
2016-06-25 13:17:27 +0700

    public tulang add userslist and specieslist menu

--- php/view/tulang.php
+++ php/view/tulang.php
@@ -110,132 +110,41 @@
             <!-- Classic list -->
             <li class="dropdown">
               <a href="#" data-toggle="dropdown" class="dropdown-toggle" aria-expanded="true">
-                List<b class="caret"></b>
+                Network<b class="caret"></b>
               </a>
               <ul class="dropdown-menu">
                 <li>
                   <!-- Content container to add padding -->
                   <div class="yamm-content">
-                    <div class="row">
-                      <ul class="col-sm-3 list-unstyled">
-                        <li>
-                          <p><strong>Section Title</strong></p>
-                        </li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                      </ul>
-                      <ul class="col-sm-3 list-unstyled">
-                        <li>
-                          <p><strong>Links Title</strong></p>
-                        </li>
-                        <li><a href="#"> Link Item </a></li>
-                        <li><a href="#"> Link Item </a></li>
-                        <li><a href="#"> Link Item </a></li>
-                        <li><a href="#"> Link Item </a></li>
-                        <li><a href="#"> Link Item </a></li>
-                        <li><a href="#"> Link Item </a></li>
-                      </ul>
-                      <ul class="col-sm-3 list-unstyled">
-                        <li>
-                          <p><strong>Section Title</strong></p>
-                        </li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                      </ul>
-                      <ul class="col-sm-3 list-unstyled">
-                        <li>
-                          <p><strong>Section Title</strong></p>
-                        </li>
-                        <li>List Item</li>
-                        <li>List Item</li>
-                        <li>
-                          <ul>
-                            <li><a href="#"> Link Item </a></li>
-                            <li><a href="#"> Link Item </a></li>
-                            <li><a href="#"> Link Item </a></li>
-                          </ul>
-                        </li>
-                      </ul>
+                    <div class="row media-body"><?php $cats = \SSBIN\Organization::getNestedArray();
+                      foreach ($cats as $cat=>$orgArr) { ?>
+                        <ul class="col-sm-2 list-unstyled">
+                          <li>
+                            <p><a href="/userslist/<?=$cat?>"><strong><?= $cat ?></strong></a></p>
+                          </li>
+                          <?php foreach ($orgArr as $org) { ?>
+                          <li><a href="/userslist/<?= "$cat/$org->name" ?>"><?=$org->name?></a></li>
+                          <?php } ?>
+                        </ul>
+                      <?php } ?>
                     </div>
                   </div>
                 </li>
               </ul>
             </li>
-            <!-- Accordion demo -->
-            <li class="dropdown">
-              <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-                Accordion<b class="caret"></b>
-              </a>
-              <ul class="dropdown-menu">
-                <li>
-                  <div class="yamm-content">
-                    <div class="row">
-                      <div id="accordion" class="panel-group">
-                        <div class="panel panel-default">
-                          <div class="panel-heading">
-                            <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Collapsible Group Item #1</a></h4>
-                          </div>
-                          <div id="collapseOne" class="panel-collapse collapse in">
-                            <div class="panel-body">Ut consectetur ullamcorper purus a rutrum. Etiam dui nisi, hendrerit feugiat scelerisque et, cursus eu magna. </div>
-                          </div>
-                        </div>
-                        <div class="panel panel-default">
-                          <div class="panel-heading">
-                            <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Collapsible Group Item #2</a></h4>
-                          </div>
-                          <div id="collapseTwo" class="panel-collapse collapse">
-                            <div class="panel-body">Nullam pretium fermentum sapien ut convallis. Suspendisse vehicula, magna non tristique tincidunt, massa nisi luctus tellus, vel laoreet sem lectus ut nibh. </div>
-                          </div>
-                        </div>
-                        <div class="panel panel-default">
-                          <div class="panel-heading">
-                            <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Collapsible Group Item #3</a></h4>
-                          </div>
-                          <div id="collapseThree" class="panel-collapse collapse">
-                            <div class="panel-body">Praesent leo quam, faucibus at facilisis id, rhoncus sit amet metus. Sed vitae ipsum non nibh mattis congue eget id augue. </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </li>
+            
             <!-- Classic dropdown -->
-            <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Classic<b class="caret"></b></a>
+            <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Species Distribution<b class="caret"></b></a>
               <ul role="menu" class="dropdown-menu">
-                <li><a tabindex="-1" href="#"> Action </a></li>
-                <li><a tabindex="-1" href="#"> Another action </a></li>
-                <li><a tabindex="-1" href="#"> Something else here </a></li>
+                <li><a tabindex="-1" href="/specieslist/class"> By Class </a></li>
+                <li><a tabindex="-1" href="/specieslist/location"> By Region </a></li>
+                <li><a tabindex="-1" href="/specieslist/landcover"> By Habitat </a></li>
+                <li><a tabindex="-1" href="/specieslist/iucn"> By IUCN Status </a></li>
                 <li class="divider"></li>
-                <li><a tabindex="-1" href="#"> Separated link </a></li>
+                <li><a tabindex="-1" href="#"> Diversity Indices </a></li>
               </ul>
             </li>
-            <!-- Pictures -->
-            <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Pictures<b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li>
-                  <div class="yamm-content">
-                    <div class="row">
-                      <div class="col-xs-6 col-sm-2"><a href="#" class="thumbnail"><img alt="150x190" src="/images/paw.png"></a></div>
-                      <div class="col-xs-6 col-sm-2"><a href="#" class="thumbnail"><img alt="150x190" src="/images/paw.png"></a></div>
-                      <div class="col-xs-6 col-sm-2"><a href="#" class="thumbnail"><img alt="150x190" src="/images/paw.png"></a></div>
-                      <div class="col-xs-6 col-sm-2"><a href="#" class="thumbnail"><img alt="150x190" src="/images/paw.png"></a></div>
-                      <div class="col-xs-6 col-sm-2"><a href="#" class="thumbnail"><img alt="150x190" src="/images/paw.png"></a></div>
-                      <div class="col-xs-6 col-sm-2"><a href="#" class="thumbnail"><img alt="150x190" src="/images/paw.png"></a></div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </li>
+            
           </ul>
         </div>
       </div>
