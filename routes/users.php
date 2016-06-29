c81b2eb732b34b218e22cedf44480f235d373f1f
great <great@DESKTOP-1UU63T4>
2016-06-29 09:45:37 +0700

    Users tambah route input data

--- routes/users.php
+++ routes/users.php
@@ -1,13 +1,10 @@
 <?php
 if (!isset($login)) { include "$ctrlDir/403.php"; die(); }
-//if (!$login->active) {
-//  if (isset($paths[1])) header("location:/users");
-//}
 
-$ctrlDir = DIR."/php/controller";
-$template = DIR."/php/view/tulang.php";
-$services = ["password","profile"];
+$ctrlDir = DIR.'/php/controller';
+$template = DIR.'/php/view/tulang.php';
+$services = ['password','profile','inputdata'];
 
-if (!isset($paths[1]) || $paths[1]=="") include "$ctrlDir/users/home.php";
+if (!isset($paths[1]) || $paths[1]=='') include "$ctrlDir/users/home.php";
 elseif (in_array($paths[1],$services)) include "$ctrlDir/users/$paths[1].php";
 else { include "$ctrlDir/404.php"; die(); }
