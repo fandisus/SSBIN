4ef1367b1d7d9a2920178f6feb5572199f21a183
great <great@DESKTOP-1UU63T4>
2016-07-10 21:09:50 +0700

    Grid, IUCN, Indo add getList method

--- php/model/Grid.php
+++ php/model/Grid.php
@@ -5,4 +5,9 @@ class Grid extends \Trust\Model{
   protected static $key_name="grid";
   protected static $table_name = "grids", $increment=false, $hasTimestamps = true;
   protected static $json_columns = ['data_info'];
+  public static function getList() {
+    $all = static::allWhere('ORDER BY grid ASC',[]);
+    foreach ($all as $k=>$v) $all[$k] = $v->grid;
+    return $all;
+  }
 }
--- php/model/IUCN_status.php
+++ php/model/IUCN_status.php
@@ -5,4 +5,9 @@ class IUCN_status extends \Trust\Model{
   protected static $key_name="abbr";
   protected static $table_name = "iucn_status", $increment=false, $hasTimestamps = true;
   protected static $json_columns = ['data_info'];
+  public static function getList() {
+    $all = static::allWhere('ORDER BY abbr ASC',[]);
+    foreach ($all as $k=>$v) $all[$k] = $v->abbr;
+    return $all;
+  }
 }
