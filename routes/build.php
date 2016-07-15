70c84c888fc9c4f310d5208bf63110e5a20891e6
great <great@DESKTOP-1UU63T4>
2016-07-15 16:13:13 +0700

    Add minifyjs to routes.

--- routes/build.php
+++ routes/build.php
@@ -3,3 +3,4 @@ $builderPath = DIR.'/php/builder';
 if ($paths[1] == "db") { include "$builderPath/database.php"; die(); }
 if ($paths[1] == 'sb') { include "$builderPath/sandbox.php"; die(); }
 if ($paths[1] == "seed") { include "$builderPath/seeder.php"; die(); }
+if ($paths[1] == 'minifyjs') { include "$builderPath/minifyjs.php"; die(); }
