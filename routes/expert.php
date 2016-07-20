3dfe37e04d75f9cb638726aa0a338a45dff5adfb
great <great@DESKTOP-1UU63T4>
2016-07-20 11:34:07 +0700

    Add taxonomies routes

--- routes/expert.php
+++ routes/expert.php
@@ -7,7 +7,10 @@ $services = [
     "users", //validate users
     "input", //Findings data validation
     ];
+$taxonServices = ['classes','families','genus','species']; //taxonomies
 
 if (!isset($paths[1]) || $paths[1]=="") include "$ctrlDir/expert/home.php";
+elseif ($paths[1]=='taxonomies' && in_array($paths[2], $taxonServices)) 
+  { include "$ctrlDir/expert/$paths[2].php"; die(); }
 elseif (in_array($paths[1], $services)) include "$ctrlDir/expert/$paths[1].php";
 else { include "$ctrlDir/404.php"; die(); }
