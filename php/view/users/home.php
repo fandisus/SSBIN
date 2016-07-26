ec62fae9304fc534ddb5197c33f01caf463b32b4
great <great@DESKTOP-1UU63T4>
2016-07-26 13:00:43 +0700

    Home: Add activation warning, warn spam folder.

--- php/view/users/home.php
+++ php/view/users/home.php
@@ -10,7 +10,7 @@ function mainContent() { global $login;
   if (!$login->active) { ?>
 <div class="alert alert-warning flex flex-vcenter" style="max-width: 500px;">
   <i class="fa fa-warning fa-fw"></i>
-  Account has not been activated. Please check email for activation link.
+  Account has not been activated. Please check email for activation link. The email might be in the spam folder.
 </div>
   <?php }
   echo "halaman home: inbox, confirmed data input<br />For experts: New validation requests";
