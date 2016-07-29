d72c65686c4ae0b23bd92e2e33e75912dfc1b3a0
great <great@DESKTOP-1UU63T4>
2016-07-29 15:46:51 +0700

    add about us module

---
+++ php/view/common/about.php
@@ -0,0 +1,29 @@
+<?php
+include DIR."/php/view/tulang.php";
+
+function htmlHead() { ?>
+<style>
+  .preface { margin: 0 auto; max-width: 500px; }
+  .preface p {
+    text-indent: 50px; text-align: justify; text-justify: inter-word;
+  }
+  .preface p:first-child {
+    text-indent: 0;
+  }
+  .preface p:first-child:first-letter {
+    float: left; color: #903;
+    font-size: 75px; line-height: 60px;
+    padding-top: 4px; padding-right: 8px; padding-left: 3px;
+    font-family: Georgia;
+  }
+
+</style>
+<?php }
+
+function mainContent() { ?>
+<div class="preface">
+<p>The objective <b>of GIZ Biodiversity and Climate Change (Bioclime)</b> is to support Indonesia's efforts to reduce greenhouse gas emissions from the forestry sector, to conserve forest biodiversity of High Value Forest Ecosystems, maintain Carbon stock storage capacities and to implement sustainable forest management for the benefit of the people in the Province of South Sumatra. </p>
+<p><b>University of Sriwijaya</b> under Department of Biological Science (Faculty of Mathematics and Natural Sciences) seeks for a Financing Agreement to support the implementation of the BIOCLIME project as to provide technical assistance to implementation of a National Biodiversity Strategy and Action Plan in South Sumatra Province (sub-national), with particular focus on: Provincial and FMU levels, Improving biodiversity knowledge and information network, Implementing tools, standards and guidelines toward integration of biodiversity into the forest management plans.</p>
+<p>Based on Project Output 2.1 to Support University of Hamburg to evaluate  best practices, benchmarking and design a biodiversity information network for the exchange of biodiversity data and for ecosystem-management in South Sumatra (aka South Sumatra Biodiversity Information Network/SSBIN).</p>
+</div>
+<?php }
