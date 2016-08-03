052c14f9959fda911a6934eea9be35b28cf38cf8
great <great@DESKTOP-1UU63T4>
2016-08-03 18:57:18 +0700

    minify js

--- public/js/common.js
+++ public/js/common.js
@@ -1,13 +1,1 @@
-printDataInfo = function(o) {
-  if (o.data_info == undefined) return '';
-  return "Created By: " + o.data_info.created_by +
-          "<br />Created At: " + moment(o.data_info.created_at).calendar() + 
-          "<br />Updated By: " + o.data_info.updated_by +
-          "<br />Updated At: " + moment(o.data_info.updated_at).calendar();
-};
-printValidationInfo = function(o) {
-  if (o.validation == undefined) return '';
-  if (!o.validation.validated) return 'Not validated yet';
-  return "Validated By: " + o.validation.validated_by +
-          "<br />Validated At: " + moment(o.validation.validated_at).calendar();
-};
+printDataInfo=function(a){return void 0==a.data_info?"":"Created By: "+a.data_info.created_by+"<br />Created At: "+moment(a.data_info.created_at).calendar()+"<br />Updated By: "+a.data_info.updated_by+"<br />Updated At: "+moment(a.data_info.updated_at).calendar()};printValidationInfo=function(a){return void 0==a.validation?"":a.validation.validated?"Validated By: "+a.validation.validated_by+"<br />Validated At: "+moment(a.validation.validated_at).calendar():"Not validated yet"};
