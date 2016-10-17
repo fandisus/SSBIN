6956afe8fb43d3eb32e0692f60136454ee8e7e6b
great <great@DESKTOP-1UU63T4>
2016-10-17 16:37:21 +0700

    Admin tulang, add pages adminisration

--- php/view/admin/tulang.php
+++ php/view/admin/tulang.php
@@ -338,6 +338,7 @@ function sidebar_search() { ?>
 function side_menus() { ?>
   <li><a href="/admin"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a></li>
   <li><a href="/admin/users"><i class="fa fa-users fa-fw"></i> Users</a></li>
+  <li><a href="/admin/pages"><i class="fa fa-file-text-o fa-fw"></i> Pages</a></li>
   <li><a href="/admin/organizations"><i class="fa fa-users fa-fw"></i> Categories / Organizations</a></li>
   <li>
     <a href="#"><i class="fa fa-table fa-fw"></i> Taxonomies <span class="fa arrow"></span></a>
