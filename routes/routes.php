631f797b49500b9a749be6b8abd3aac8c4364756
great <great@DESKTOP-1UU63T4>
2016-10-19 14:20:41 +0700

    Disable build routes.

--- routes/routes.php
+++ routes/routes.php
@@ -3,6 +3,6 @@ switch ($paths[0]) {
  case 'admin': include 'admin.php';break;
  case 'users': include 'users.php';break;
  case 'expert': include 'expert.php';break;
- case 'build': include 'build.php';break; //TODO: Comment this on production
+ //case 'build': include 'build.php';break; //TODO: Comment this on production
  default: include 'common.php'; break;
 }
